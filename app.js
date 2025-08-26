c3c9f8c29b955af45617ededbca9a2d04ad1a99a
JefersonBLuz <jefersonbrluz@gamil.com>
2025-08-26 02:14:56 -0300

    feat: Implementa validação de nome e atualiza lista

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 const amigos = [];
+let errorMessage = "";
+
+const nameValid = (name) => {
+  if(name.trim() === ""){
+    errorMessage = "Por favor, insira um nome.";
+    return false;
+  }
+
+  if (amigos.includes(name.trim())) {
+    errorMessage = "Nome já existe";
+    return false;
+  }
+
+  return true;
+};
+
+function adicionarAmigo() {
+  const name = document.getElementById("amigo").value;
+  if (nameValid(name)) {
+    amigos.push(name);
+    document.getElementById("amigo").value = "";
+    listarAmigos();
+    return;
+  }
+  alert(errorMessage);
+};
+
+const listarAmigos = () => {
+  const listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+  amigos.forEach((amigo) => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
+    listaAmigos.appendChild(li);
+  });
+};
