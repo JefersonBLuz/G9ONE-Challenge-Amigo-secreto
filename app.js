88508a5ae9c105d554088f0bfb19aefc3599a5a7
JefersonBLuz <jefersonbrluz@gamil.com>
2025-08-27 11:46:55 -0300

    feat: Adiciona funções para desabilitar botão de sortear e listar amigos, além de melhorias na exibição de mensagens de erro no CSS

--- app.js
+++ app.js
@@ -5,25 +5,30 @@ let errorMessage = "";
 const errorMessageElement = document.getElementById("error-message");
 const resultado = document.getElementById("resultado");
 
+//Eventos
+document.addEventListener("DOMContentLoaded", () => {
+  desabilitarBotaoSortear();
+});
 
-
+document.addEventListener("click", (event) => {
+  desabilitarBotaoSortear();
+});
 
 //Funções
-
+//Função para validar o nome do amigo
 const nameValid = (name) => {
   if(name.trim() === ""){
     errorMessage = "Por favor, insira um nome.";
     return false;
   }
-
   if (amigos.includes(name.trim())) {
     errorMessage = "Nome já existe";
     return false;
   }
-
   return true;
 };
 
+//Função para adicionar o amigo na lista
 const adicionarAmigo = () => {
   const name = document.getElementById("amigo").value;
   limparDados();
@@ -36,6 +41,7 @@ const adicionarAmigo = () => {
   errorMessageElement.textContent = errorMessage;
 };
 
+//Função para listar os amigos na lista
 const listarAmigos = () => {
   const listaAmigos = document.getElementById("listaAmigos");
   listaAmigos.innerHTML = "";
@@ -46,6 +52,7 @@ const listarAmigos = () => {
   });
 };
 
+//Função para sortear o amigo secreto
 const sortearAmigo = () => {
   if (amigos.length > 1) {
     const amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
@@ -53,13 +60,25 @@ const sortearAmigo = () => {
   }
 };
 
+//Função para limpar os dados da lista
 const limparDados = () => {
   errorMessageElement.textContent = "";
   resultado.textContent = "";
 };
 
+//Função para limpar a lista
 const limparLista = () => {
   amigos.length = 0;
   listarAmigos();
   limparDados();
+};
+
+//Função para desabilitar o botão de sortear
+const desabilitarBotaoSortear = () => {
+  const buttonSortear = document.querySelector(".button-draw");
+  if (amigos.length < 2) {
+    buttonSortear.disabled = true;
+  } else {
+    buttonSortear.disabled = false;
+  }
 };
